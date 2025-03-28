c91ab6450892f348c2ab22d189f93a02be9cafce
Anoop Kumar Burnwal <anoopkumar19062006@gmail.com>
2025-03-29 00:19:56 +0530

    Modified

--- 06_currency_convertor/src/hooks/useCurrencyInfo.jsx
+++ 06_currency_convertor/src/hooks/useCurrencyInfo.jsx
@@ -3,7 +3,6 @@ import {useEffect , useState} from 'react';
 function useCurrencyInfo(currency){
     const [data , setData] = useState({});
     useEffect(()=>{
-
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => {
             if (!res.ok) {
